86d232972d9e9b976315a87259e70bdd006af890
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 20:25:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-/*function forLoop( array ) {
-  for( i = 0, i < 25, i++ ) {
+function forLoop( array ) {
+  for( let i = 0, i < 25, i++ ) {
       if( i === 1 ){
         array.push( `I am 1 strange loop.` )
       } else {
@@ -7,11 +7,8 @@
       }
   }
   return array
-}*/
-
-function forLoop( array ) {
-  return array
 }
+
 
 function whileLoop( n ) {
   return n
